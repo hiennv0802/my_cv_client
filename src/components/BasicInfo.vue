bf13a9df97035d8fb278634f211ec2070276fd8b
hiennv0802 <hiennv.th@gmail.com>
2019-04-18 23:30:40 +0700

    Basic info

---
+++ src/components/BasicInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>NGUYEN VAN HIEN</h1>
+    <p>Nếu đã làm việc gì, hãy phấn đấu là người số 1 lĩnh vực đó, không bao giờ thoả hiệp với vị trí thứ 2.</p>
+    <p>Hãy phấn đấu hết mình, cho dù may mắn có thể không đến với bạn nhưng chắc chắn bạn sẽ không phải hối tiếc về quãng thời gian đó.</p>
+    <div class="row">
+      <div class="col-sm-5">
+        <img src="../assets/user-avatar.png" class="user-avatar">
+      </div>
+      <div class="col-sm-7">
+        <button class="btn btn-primary">Click</button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicInfo"
+}
+</script>
+
+<style>
+.user-avatar {
+  width: 200px;
+  height: 335px;
+}
+</style>
